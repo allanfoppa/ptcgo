6b03468fe1dc8326481e9998f94d800220261b3b
Allan Foppa <allanfoppa@hotmail.com>
2025-01-21 14:16:38 -0300

    Security issues

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,16 +7,31 @@ import { ResponseHelper } from './common/helpers/response/response.helper';
 import { EnvironmentModule } from './common/services/enviroments-variables/enviroments-variables.module';
 import { MetadataModule } from './common/helpers/metadata/metadata.module';
 import { LoggerModule } from './common/logger/logger.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
     EnvironmentModule,
     MetadataModule,
     LoggerModule,
   ],
   controllers: [AppController],
-  providers: [AppService, ResponseHelper],
+  providers: [
+    AppService,
+    ResponseHelper,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
+import helmet from 'helmet';
 
 export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(helmet());
   app.enableCors();
   app.useGlobalInterceptors(new ResponseInterceptor());
 
