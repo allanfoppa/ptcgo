82eda316f8dc9e74006c512e2bf5c0dff680b9ba
Allan Foppa <allanfoppa@hotmail.com>
2025-02-03 15:14:22 -0300

    Zod validation response refactor

--- backend/src/common/pipes/zod-validation/zod-validation.pipe.ts
+++ backend/src/common/pipes/zod-validation/zod-validation.pipe.ts
@@ -1,7 +1,5 @@
-import { PipeTransform, HttpStatus } from '@nestjs/common';
+import { PipeTransform, BadRequestException } from '@nestjs/common';
 import { ZodSchema } from 'zod';
-import { response } from 'express';
-import { STATUS } from '../../enums/status.enum';
 
 export class ZodValidationPipe implements PipeTransform {
   constructor(private schema: ZodSchema) {}
@@ -12,17 +10,7 @@ export class ZodValidationPipe implements PipeTransform {
 
       return parsedValue;
     } catch (error) {
-      return response.status(HttpStatus.OK).json({
-        metadata: {
-          status: STATUS.SUCCESS,
-          statusCode: HttpStatus.BAD_REQUEST,
-          message: `The attribute ${error.errors[0].path[0]}: ${error.errors[0].message}`,
-          info: {
-            path: '/',
-            timestamp: new Date().toISOString(),
-          },
-        },
-      });
+      throw new BadRequestException(error.errors[0].message);
     }
   }
 }
