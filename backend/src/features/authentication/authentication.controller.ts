59c6abe7d7787442d6d08151e7de0099446b37a7
Allan Foppa <allanfoppa@hotmail.com>
2025-02-03 15:55:30 -0300

    Refactoring the service

--- backend/src/features/authentication/authentication.controller.ts
+++ backend/src/features/authentication/authentication.controller.ts
@@ -1,80 +1,24 @@
-import {
-  Body,
-  Controller,
-  HttpStatus,
-  Post,
-  Res,
-  UsePipes,
-} from '@nestjs/common';
+import { Body, Controller, Post, UsePipes } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
-import { Response } from 'express';
 import {
   AuthenticationDto,
   authenticationSchema,
 } from './dto/authentication.dto';
 import { ZodValidationPipe } from 'src/common/pipes/zod-validation/zod-validation.pipe';
-import { ValidatePasswordHelper } from 'src/common/helpers/validate-password/validate-password.helper';
-import { STATUS } from 'src/common/enums/status.enum';
-import { GetUserByUserNameHelper } from 'src/common/helpers/database/get-user-by-username/get-user-by-username.helper';
+import { ResponseCatchHelper } from 'src/common/helpers/response-catch/response-catch.helper';
 
 @Controller('authentication')
 export class AuthenticationController {
-  constructor(
-    private readonly authenticationService: AuthenticationService,
-    private readonly validatePassword: ValidatePasswordHelper,
-    private readonly getUserByUserName: GetUserByUserNameHelper,
-  ) {}
+  constructor(private readonly authenticationService: AuthenticationService) {}
 
   @Post()
   @UsePipes(new ZodValidationPipe(authenticationSchema))
-  async auth(
-    @Res() response: Response,
-    @Body() authenticationDto: AuthenticationDto,
-  ) {
-    const { password } = await this.getUserByUserName.getUser(
-      authenticationDto.username,
-    );
-    console.log(password);
-
-    const isPasswordValid = await this.validatePassword.compare({
-      password: authenticationDto.password,
-      hashedPassword: password,
-    });
-
-    console.log(isPasswordValid);
-    if (!isPasswordValid) {
-      return response.status(HttpStatus.UNAUTHORIZED).json({
-        metadata: {
-          status: STATUS.ERROR,
-          statusCode: HttpStatus.UNAUTHORIZED,
-          message: 'Invalid credentials.',
-          info: {
-            path: '/authentication',
-            timestamp: new Date().toISOString(),
-          },
-        },
-        content: {},
-      });
+  async auth(@Body() authenticationDto: AuthenticationDto) {
+    try {
+      console.log(authenticationDto);
+      return this.authenticationService.auth(authenticationDto);
+    } catch (error) {
+      ResponseCatchHelper.catch(error.message);
     }
-
-    return response.status(HttpStatus.UNAUTHORIZED).json({
-      metadata: {
-        status: STATUS.ERROR,
-        statusCode: HttpStatus.UNAUTHORIZED,
-        message: 'Invalid credentials.',
-        info: {
-          path: '/authentication',
-          timestamp: new Date().toISOString(),
-        },
-      },
-      content: {
-        user: {
-          name: 'John Doe',
-        },
-      },
-    });
-
-    console.log(authenticationDto);
-    return this.authenticationService.auth();
   }
 }
--- backend/src/features/authentication/authentication.service.ts
+++ backend/src/features/authentication/authentication.service.ts
@@ -1,8 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { GetUserByUserNameHelper } from 'src/common/helpers/database/get-user-by-username/get-user-by-username.helper';
+import { ValidatePasswordHelper } from 'src/common/helpers/validate-password/validate-password.helper';
+import { AuthenticationDto } from './dto/authentication.dto';
 
 @Injectable()
 export class AuthenticationService {
-  async auth() {
-    return 'auth';
+  constructor(
+    private readonly validatePassword: ValidatePasswordHelper,
+    private readonly getUserByUserName: GetUserByUserNameHelper,
+  ) {}
+
+  async auth(authenticationDto: AuthenticationDto) {
+    const user = await this.getUserByUserName.getUser(
+      authenticationDto.username,
+    );
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials.');
+    }
+
+    const isPasswordValid = await this.validatePassword.compare({
+      password: authenticationDto.password,
+      hashedPassword: user.password,
+    });
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials.');
+    }
+
+    return;
   }
 }
