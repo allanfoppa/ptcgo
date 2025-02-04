868c9dc7ddfd1a3c969b827279421b65783d9b71
Allan Foppa <allanfoppa@hotmail.com>
2025-02-04 13:51:05 -0300

    Transfer logic to service

--- backend/src/features/registration/registration.controller.ts
+++ backend/src/features/registration/registration.controller.ts
@@ -1,45 +1,25 @@
-import {
-  Controller,
-  Post,
-  Body,
-  UsePipes,
-  ConflictException,
-} from '@nestjs/common';
+import { Controller, Post, Body, UsePipes } from '@nestjs/common';
 import { RegistrationService } from './registration.service';
 import {
   registrationSchema,
   RegistrationDto,
 } from './dto/create-registration.dto';
 import { ZodValidationPipe } from 'src/common/pipes/zod-validation/zod-validation.pipe';
-import { IsUsernameExistsService } from '../../common/helpers/database/is-username-exists/is-username-exists.service';
 import { ResponseHelper } from 'src/common/helpers/response/response.helper';
 import { ResponseCatchHelper } from 'src/common/helpers/response-catch/response-catch.helper';
 import { Public } from 'src/common/decorators/public.decorator';
 
 @Controller('registration')
 export class RegistrationController {
-  constructor(
-    private readonly registrationService: RegistrationService,
-    private readonly isUsernameExistsService: IsUsernameExistsService,
-  ) {}
+  constructor(private readonly registrationService: RegistrationService) {}
 
   @Post()
   @Public()
   @UsePipes(new ZodValidationPipe(registrationSchema))
   async registration(@Body() registrationDto: RegistrationDto) {
     try {
-      const isUserExists = await this.isUsernameExistsService.isUserExists(
-        registrationDto.username,
-      );
-
-      if (isUserExists) {
-        throw new ConflictException('Username is already in use.');
-      }
-
-      const registrationResponse =
+      const { id, username } =
         await this.registrationService.registration(registrationDto);
-
-      const { id, username } = registrationResponse;
 
       return ResponseHelper.success({
         message: 'Registration successful',
--- backend/src/features/registration/registration.service.ts
+++ backend/src/features/registration/registration.service.ts
@@ -1,16 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { RegistrationDto } from './dto/create-registration.dto';
 import { HashingHelper } from 'src/common/helpers/hashing/hashing.helper';
 import { RegistrationRepository } from './registration.repository';
+import { IsUsernameExistsService } from 'src/common/helpers/database/is-username-exists/is-username-exists.service';
 
 @Injectable()
 export class RegistrationService {
   constructor(
     private readonly hashingHelper: HashingHelper,
     private readonly registrationRepository: RegistrationRepository,
+    private readonly isUsernameExistsService: IsUsernameExistsService,
   ) {}
 
   async registration(registrationDto: RegistrationDto) {
+    const isUserExists = await this.isUsernameExistsService.isUserExists(
+      registrationDto.username,
+    );
+
+    if (isUserExists) {
+      throw new ConflictException('Username is already in use.');
+    }
+
     registrationDto.password = await this.hashingHelper.hashPassword({
       password: registrationDto.password,
     });
