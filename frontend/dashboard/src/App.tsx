73e0b95096c0083ece83c623cddd0947bf221ba4
Allan Foppa <allanfoppa@hotmail.com>
2025-03-20 07:58:45 -0300

    Remove context from dashboard App

--- frontend/dashboard/src/App.tsx
+++ frontend/dashboard/src/App.tsx
@@ -1,16 +1,8 @@
-import { useContext } from 'react';
-import { GlobalContext } from 'core/GlobalContext'
-import { PlaceholderText } from 'core/Placeholder'
-
 const Dashboard = () => {
-
-  const { text }: { text: string } = useContext(GlobalContext);
 
   return (
     <div>
-      <b>DASHBOARD:</b>
-      <br />
-      {text || <PlaceholderText />}
+      <h1>DASHBOARD</h1>
     </div>
   );
 };
